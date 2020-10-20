7dc867e573365a484bc344fb4d025921d1e37a0c
Santiago Torres G <santiago.x.torres.-nd@disney.com>
2020-10-19 22:22:54 -0500

    Update prject structure, separting of concerns

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 'use strict'
 
 const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
 // Defining schema
-const schema = buildSchema(`
-    type Query {
-        hello: String
-        greeting: String
-    }
-`)
-
-// Setting up Resolvers
-
-const resolvers = {
-  hello: () => 'Hello world',
-  greeting: () => 'Hey everyone!'
-}
+const schema = buildSchema(
+  readFileSync(
+    join('./', 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
 app.use('/api', graphqlHTTP({
   schema,
