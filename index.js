ccbbb3eeab718d898b275926a8808c116a10b5a5
Santiago Torres G <santiago.x.torres.-nd@disney.com>
2020-10-19 22:45:34 -0500

    Add query that uses arguments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const express = require('express')
-const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -11,12 +11,12 @@ const app = express()
 const port = process.env.port || 3000
 
 // Defining schema
-const schema = buildSchema(
-  readFileSync(
-    join('./', 'lib', 'schema.graphql'),
-    'utf-8'
+const typeDefs = readFileSync(
+  join('./', 'lib', 'schema.graphql'),
+  'utf-8'
   )
-)
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', graphqlHTTP({
   schema,
