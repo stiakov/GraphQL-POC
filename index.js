9af93b1ce3807c1a7b9861da6835908cca2c83b9
Santiago Torres G <santiago.x.torres.-nd@disney.com>
2020-10-19 21:44:04 -0500

    Add Resolvers

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
 
 const { graphql, buildSchema } = require('graphql')
 
-
 // Defining schema
 const schema = buildSchema(`
     type Query {
         hello: String
+        greeting: String
     }
 `)
 
+// Setting up Resolvers
+
+const resolvers = {
+  hello: () => 'Hello world',
+  greeting: () => 'Hey everyone!',
+}
+
 // Executing the hello query
-graphql(schema, '{ hello }')
-    .then(data => console.log(data));
+graphql(schema, '{ greeting }', resolvers)
+  .then(data => console.log(data))
